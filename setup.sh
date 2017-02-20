9fef440826c7dc255950ee9123d7ca9c7c70a611
Jason Nance <jnance@rti.org>
2017-02-20 11:48:10 -0500

    WIP: Add custom snippets
    Symlink'd directory isn't being created properly at the moment.

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ function get_abs_filename() {
     echo "$(cd "$(dirname "$rel_filename")" && pwd)/$(basename "$rel_filename")"
 }
 
+# Setup dotfiles
 for dotfile in .gitignore_global .gvimrc .vimrc .spacemacs .vim .ipyrc; do
     if [[ ! -e "../$dotfile" ]]; then
         ln -s $(get_abs_filename "$dotfile") "$HOME/$dotfile"
@@ -20,3 +21,13 @@ for dotfile in .gitignore_global .gvimrc .vimrc .spacemacs .vim .ipyrc; do
         fi
     fi
 done
+
+# Setup custom emacs snippets
+# TODO not working
+base_snippet_dir="$HOME/.spacemacs.d/snippets"
+mkdir -p "$base_snippet_dir"
+for snippet_dir in $(ls ./snippets); do
+    pushd "$base_snippet_dir" > /dev/null
+    ln -s $(get_abs_filename "$snippet_dir") "$snippet_dir"
+    popd > /dev/null
+done;
